ed4db1f3f202b384d471d128ab3cceff1760d7c5
Gopalksaravanan <sgopalmechanic@gmail.com>
2022-11-07 17:28:10 +0530

    message

---
+++ src/main/java/org/gittask/GitTask.java
@@ -0,0 +1,15 @@
+package org.gittask;
+
+public class GitTask {
+	
+	public void india() {
+		
+		System.out.println("Enter into semi 1st");
+	}
+
+	public void pakistan() {
+		
+		System.out.println("Enter into semi 2nd");
+
+	}
+}
